c656fb3438fdb6a142e7b2737e907c41f5949e04
Prashanti Nilayam <prashanti.nilayam@servicenow.com>
2021-05-07 10:56:57 -0700

    Model changes

--- mimic3models/length_of_stay/cs598/model.py
+++ mimic3models/length_of_stay/cs598/model.py
@@ -6,19 +6,19 @@ import torch.nn.functional as F
 class EpisodeCNN(nn.Module):
     def __init__(self):
         super(EpisodeCNN, self).__init__()
-        #input shape 1 * 17 * 4
-        #output shape 17 * 17 * 4
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=17, kernel_size=3, padding=1, stride = 1)
-        #input shape 17 * 17 * 4
-        #output shape 68 * 17 * 4
-        self.conv2 = nn.Conv2d(in_channels=17, out_channels=34, kernel_size=3, padding=1, stride = 1)
-        #input shape 34 * 17 * 4
-        #output shape 34 * 8 * 2
+        #input shape 34 * 8
+        #output shape 16 * 8
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, padding=1, stride = 1)
+        #input shape 16 * 8
+        #output shape 32 * 8
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1, stride = 1)
+        #input shape 32 * 8
+        #output shape 16 * 4
         self.pool1 = nn.MaxPool2d(2,2)
-        #input shape 34 * 8 * 2
-        #output shape 68 * 8 * 2
-        self.conv3 = nn.Conv2d(in_channels=34, out_channels=68, kernel_size=3, padding=1, stride = 1)
-        self.fc1 = nn.Linear(68*8*2, 256)
+        #input shape 16 * 4
+        #output shape 32 * 4
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1, stride = 1)
+        self.fc1 = nn.Linear(64*17*4, 256)
         self.dropout = nn.Dropout(0.5)
         self.fc2 = nn.Linear(256, 128)
         self.fc3 = nn.Linear(128, 32)
@@ -26,15 +26,23 @@ class EpisodeCNN(nn.Module):
 
     def forward(self, x):
         #input is of shape (batch_size=32, 3, 224, 224) if you did the dataloader right
+        #input is of shape (batch_size=32, 3, 224, 224) if you did the dataloader right
         x = x.unsqueeze(1)
+        #print(x.shape)
         x = F.leaky_relu(self.conv1(x))
+        #print(x.shape)
         x = F.leaky_relu(self.conv2(x))
+        #print(x.shape)
         x = self.pool1(x)
+        #print(x.shape)
         x = F.leaky_relu(self.conv3(x))
-        x = x.view(-1, 68 * 8 * 2)
+        #print(x.shape)
+        x = x.view(-1, 64*17*4)
         x = F.leaky_relu(self.fc1(x))
         x = self.dropout(x)
+        #print(x.shape)
         x = F.leaky_relu(self.fc2(x))
+        #print(x.shape)
         x = F.leaky_relu(self.fc3(x))
         x = self.fc4(x)
         return x
